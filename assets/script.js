c8b52f4066f618ba1dbc81f02415bf6376024e0f
l-amu <lamiekgim@gmail.com>
2021-10-24 12:39:21 -0400

    stuck in multiple choices

--- assets/script.js
+++ assets/script.js
@@ -1,69 +1,117 @@
 var startEl = document.querySelector("#start-quize");
 var viewHighScoreEl = document.querySelector("#highscore-Btn")
 var timerEl = document.getElementById("countdown")
-
+var questionHolder = document.getElementById('questions');
+var answerEl = document.getElementById('answers')
 var quizeData = [
     {
         question: "Commonly used data types Do not include:",
         choices: ["strings", "booleans", "alerts", "numbers"],
         answer: "alerts"
-  
-      },
-      {
+
+    },
+    {
         question: "The condtion in an if/else statment is enclosed with______.",
         choices: ["quotes", "curly brackets", "paranthesis", "square brackets"],
         answer: "paranthesis"
-      },
-      {
+    },
+    {
         question: "Array in javascript can be used to store___________.",
         choices: ["number and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
-      },
-      {
+    },
+    {
         question: "String values must be enclosed within_________when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "paranthesis"],
         answer: "quotes"
-      },
-      {
+    },
+    {
         question: "A very useful tool used during development and debugging for printing content to the debugger is",
         choices: ["javascript", "terminal/bash", "for loops", "console.log"],
         answer: "console.log"
-      },
-      {
+    },
+    {
         question: "String values must be enclosed within_________when being assigned to variables.",
         choices: ["commas", "curly brackets", "quotes", "paranthesis"],
         answer: "quotes"
-      }    
-  ]
+    }
+]
 
-  var timeLeft = 10;
-  var currentQuestion = 0;
+var timerInnerText = 70;
+var score = 0
+var highScore = []
+var questionCounter = 0;
+var currentChoices = 0;
 
-  function startQuize() {
+
+function startQuize() {
     timer();
     generateQuestion();
-  };
+};
+//   check the statues of the game concurrent to the time
+function timer() {
+    var timeInterval = setInterval(function () {
+        if (timerInnerText >= 60) {
+            timerInnerText--;
+            timerEl.textContent = "Time: " + timerInnerText;
+            
 
-  function timer() {
-    var timeInterval = setInterval(function() {
-        if (timeLeft > 1) {
-            timeLeft--;
-            timerEl.textContent = "Time: " + timeLeft;          
+
         } else {
+            // alert ("game over");
             clearInterval(timeInterval);
         }
-      }, 1000);
-      console.log("function", timeInterval)
-  };
+    }, 1000);
+    console.log("timer", timeInterval)
+};
 
-//   function generateQuestion() {
-//     currentQuestion = quizeData[0]
-//     quizeData.forEach(question, 0 => console.log("element", quizeData));
+function generateQuestion() {
+    
+//     
+//     displayQuestions.textContent = questions[currentQuestion]
+    var hideStartQuize = document.querySelector('.start-holder');
+    hideStartQuize.classList.add('hide');
 
-      
-//   };
+//     questionHolder.appendChild(displayQuestions);
+//     
+//    quizeData.forEach
+    var currentQuestion = quizeData[questionCounter]
+    var displayQuestions = document.createElement('h2');
+    displayQuestions.textContent = currentQuestion.question;
+    questionHolder.appendChild(displayQuestions);
+    questionHolder.classList.remove('hide');
+    console.log(displayQuestions);
 
-  
+
+    multipleChoices.textContent = currentQuestion.answer
+
+    var multipleChoices = document.createElement('button') 
+    answerEl.appendChild(multipleChoices)
 
 
-startEl.addEventListener('click', startQuize)  
+    
+
+};
+
+function checkAnswer (event) {
+    var correctAnswer = question[questionCounter].answer
+    var currentAnswer = event.taget.textContent
+
+    if (currentAnswer === correctAnswer) {
+        alert("correctAnswer")     
+    } else {
+        alert("wrong")
+    }
+
+}
+
+startEl.addEventListener('click', startQuize)
+answerEl.addEventListener('click', checkAnswer)
+
+
+
+
+
+
+
+
